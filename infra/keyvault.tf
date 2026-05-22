b75feadbb7d03540578fe296df00bfa162893b04
You have one new message <george.trombley6000@gmail.com>
2026-05-22 15:06:12 +0200

    Update keyvault.tf

--- infra/keyvault.tf
+++ infra/keyvault.tf
@@ -1,11 +1,26 @@
 # keyvault.tf
 
 resource "azurerm_key_vault" "foundry" {
-  name                = "kv-foundry-maf-dev"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
+  name                       = "kv-foundry-maf-dev"
+  resource_group_name        = azurerm_resource_group.main.name
+  location                   = azurerm_resource_group.main.location
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  enable_rbac_authorization  = true
 
   tags = azurerm_resource_group.main.tags
+}
+
+# Foundry hub can read/write secrets
+resource "azurerm_role_assignment" "foundry_hub_kv" {
+  scope                = azurerm_key_vault.foundry.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = azurerm_ai_foundry.hub.identity[0].principal_id
+}
+
+# Your own identity (deployer) gets full admin access
+resource "azurerm_role_assignment" "admin_kv" {
+  scope                = azurerm_key_vault.foundry.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = var.admin_object_id
 }

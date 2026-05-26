1cf298e99900f7c51400753e405079645e304219
You have one new message <george.trombley6000@gmail.com>
2026-05-26 15:44:00 +0200

    Update keyvault.tf

--- infra/keyvault.tf
+++ infra/keyvault.tf
@@ -1,7 +1,7 @@
 # keyvault.tf
 
 resource "azurerm_key_vault" "foundry" {
-  name                = "kv-foundry-maf-dev"
+  name                = "kv-happyliving-dev"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   tenant_id           = data.azurerm_client_config.current.tenant_id

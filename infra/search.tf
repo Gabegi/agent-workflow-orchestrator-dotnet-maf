1e317e595b61d7c952e10f72570786efed260368
You have one new message <george.trombley6000@gmail.com>
2026-05-26 09:35:16 +0200

    Update search.tf

--- infra/search.tf
+++ infra/search.tf
@@ -3,7 +3,7 @@
 resource "azurerm_search_service" "main" {
   name                = "srch-workflow-maf-dev"
   resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  location            = "eastus"
   sku                 = "basic"
 
   tags = azurerm_resource_group.main.tags
